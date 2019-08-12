9fec75707f437c27c2fac0786d35be223d1665aa
Jordan Lambrecht <jordan@pixelbakery.co>
2019-08-12 09:28:53 -0500

    fixed when the sun is up. removed weather stuff for now.

--- partials/functions.php
+++ partials/functions.php
@@ -37,7 +37,7 @@ $arr_time = DateTime::createFromFormat('H:i', $current_time);
 $arr_set = DateTime::createFromFormat('H:i', $sunset);
 $arr_dusk = date('H', $arr_set);
 $arr_rise = DateTime::createFromFormat('H:i', $sunrise);
- if ($arr_time > $arr_set && $arr_time < $arr_rise)
+ if ($arr_time > $arr_rise && $arr_time < $arr_set)
 {
    echo 'the sun is up.</br>';
 }
@@ -54,38 +54,3 @@ else {
     echo "<br>Dusk End time: " . date('H:i',strtotime('+85 minutes',strtotime($sunset)));
     echo "<br>Dawn Start time: " . date('H:i',strtotime('-85 minutes',strtotime($sunrise)));
   }
-
-
-
-
-/*
-created by Kasper Lorentzen 2017
-*/
-$cityName = filter_input(INPUT_GET,'city',FILTER_SANITIZE_STRING);
-if (!isset($cityName)) {
-	$cityName = 'Sydney';
-}
-$country = filter_input(INPUT_GET,'country',FILTER_SANITIZE_STRING);
-if (!isset($country)) {
-	$country = 'AU';
-}
-$appId = 'c5fd894625a814e1d46c44ada1ad46e6';
-$rainUrlFormat = 'http://api.openweathermap.org/data/2.5/weather?q=%s,%s&units=metric&appid=%s';
-$rainUrl = sprintf($rainUrlFormat, $cityName, $country, $appId);
-$ch = curl_init();
-// set URL and other appropriate options
-curl_setopt($ch, CURLOPT_URL, $rainUrl);
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER , 1);
-// grab URL and pass it to the browser
-$json = curl_exec($ch);
-// close cURL resource, and free up system resources
-curl_close($ch);
-$data = json_decode($json,true);
-$rain = strtolower($data["weather"][0]["main"]);
-//print $rain;
-$pos = strpos($note, "rain");
-if ($pos !== false)
-	print "no";
-else print "It is raining.";
-//var_dump($rainData);
